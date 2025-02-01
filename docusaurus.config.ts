633b7ee90b695fe066ca263b1ba78eebd7922472
alaninnovates <alanlichen1@gmail.com>
2025-01-31 20:07:00 -0800

    Configure feature section and footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,147 +1,151 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-	title: 'Eaglescout',
-	tagline: 'Dinosaurs are cool',
-	favicon: 'img/favicon.ico',
+    title: 'EagleScout',
+    tagline: 'Next Generation Scouting for FRC',
+    favicon: 'img/favicon.ico',
 
-	// Set the production url of your site here
-	url: 'https://eaglescout.github.io',
-	// Set the /<baseUrl>/ pathname under which your site is served
-	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: '/docs/',
+    // Set the production url of your site here
+    url: 'https://eaglescout.github.io',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/docs/',
 
-	// GitHub pages deployment config.
-	// If you aren't using GitHub pages, you don't need these.
-	organizationName: 'eaglescoutdev', // Usually your GitHub org/user name.
-	projectName: 'docs', // Usually your repo name.
-	trailingSlash: false,
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'eaglescoutdev', // Usually your GitHub org/user name.
+    projectName: 'docs', // Usually your repo name.
+    trailingSlash: false,
 
-	onBrokenLinks: 'throw',
-	onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-	// Even if you don't use internationalization, you can use this field to set
-	// useful metadata like html lang. For example, if your site is Chinese, you
-	// may want to replace "en" with "zh-Hans".
-	i18n: {
-		defaultLocale: 'en',
-		locales: ['en'],
-	},
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
 
-	presets: [
-		[
-			'classic',
-			{
-				docs: {
-					sidebarPath: './sidebars.ts',
-				},
-				blog: {
-					showReadingTime: true,
-					feedOptions: {
-						type: ['rss', 'atom'],
-						xslt: true,
-					},
-					// Useful options to enforce blogging best practices
-					onInlineTags: 'warn',
-					onInlineAuthors: 'warn',
-					onUntruncatedBlogPosts: 'warn',
-				},
-				theme: {
-					customCss: './src/css/custom.css',
-				},
-			} satisfies Preset.Options,
-		],
-	],
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                },
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: ['rss', 'atom'],
+                        xslt: true,
+                    },
+                    // Useful options to enforce blogging best practices
+                    onInlineTags: 'warn',
+                    onInlineAuthors: 'warn',
+                    onUntruncatedBlogPosts: 'warn',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
 
-	themeConfig: {
-		colorMode: {
-			defaultMode: "dark",
-		},
-		navbar: {
-			title: 'Eaglescout',
-			logo: {
-				alt: 'Eaglescout',
-				src: 'img/app_icon.webp',
-			},
-			items: [
-				{
-					to: '/leads',
-					type: 'docSidebar',
-					sidebarId: 'leadsSidebar',
-					position: 'left',
-					label: 'For Leads',
-				},
-				{
-					to: '/scouters',
-					type: 'docSidebar',
-					sidebarId: 'scoutersSidebar',
-					position: 'left',
-					label: 'For Scouters',
-				},
-				{to: '/blog', label: 'Blog', position: 'left'},
-			],
-		},
-		footer: {
-			style: 'dark',
-			links: [
-				{
-					title: 'Docs',
-					items: [
-						{
-							label: 'Tutorial',
-							to: 'docs/leads/home',
-						},
-					],
-				},
-				{
-					title: 'Community',
-					items: [
-						{
-							label: 'Stack Overflow',
-							href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-						},
-						{
-							label: 'Discord',
-							href: 'https://discordapp.com/invite/docusaurus',
-						},
-						{
-							label: 'X',
-							href: 'https://x.com/docusaurus',
-						},
-					],
-				},
-				{
-					title: 'More',
-					items: [
-						{
-							label: 'Blog',
-							to: '/blog',
-						},
-						{
-							label: 'GitHub',
-							href: 'https://github.com/facebook/docusaurus',
-						},
-					],
-				},
-			],
-			copyright: `Copyright © ${new Date().getFullYear()} Eaglescout`,
-		},
-		prism: {
-			theme: prismThemes.github,
-			darkTheme: prismThemes.dracula,
-		},
-		zoom: {
-			selector: '.markdown > img',
-		},
-	} satisfies Preset.ThemeConfig,
+    themeConfig: {
+        colorMode: {
+            defaultMode: 'dark',
+        },
+        navbar: {
+            title: 'EagleScout',
+            logo: {
+                alt: 'Eaglescout',
+                src: 'img/app_icon.webp',
+            },
+            items: [
+                {
+                    to: '/leads',
+                    type: 'docSidebar',
+                    sidebarId: 'leadsSidebar',
+                    position: 'left',
+                    label: 'For Leads',
+                },
+                {
+                    to: '/scouters',
+                    type: 'docSidebar',
+                    sidebarId: 'scoutersSidebar',
+                    position: 'left',
+                    label: 'For Scouters',
+                },
+                { to: '/blog', label: 'Blog', position: 'left' },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'For Leads',
+                            to: 'docs/leads/home',
+                        },
+                        {
+                            label: 'For Scouters',
+                            to: 'docs/scouters/home',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Discord',
+                            href: 'https://discord.com',
+                        },
+                        {
+                            label: 'Instagram',
+                            href: 'https://instagram.com',
+                        },
+                        {
+                            label: 'Github',
+                            href: 'https://github.com/eaglescoutdev/eaglescout',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'Blog',
+                            to: '/blog',
+                        },
+                        {
+                            label: 'Eaglescout Homepage',
+                            href: 'https://eaglescout.one',
+                        }
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Eaglescout`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+        zoom: {
+            selector: '.markdown > img',
+        },
+    } satisfies Preset.ThemeConfig,
 
-	plugins: [
-		"docusaurus-plugin-image-zoom",
-	]
+    plugins: [
+        'docusaurus-plugin-image-zoom',
+    ],
 };
 
 export default config;
--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,71 +1,90 @@
-import type {ReactNode} from 'react';
+import type { ReactNode } from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
 type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: ReactNode;
+    title: string;
+    description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+    {
+        title: 'Mobile-First',
+        description: (
+            <>
+                Crush the competition with EagleScout's simple yet pleasing UI, providing ease of observation and
+                tracking. With our impeccable mobile compatibility, focus on the match and forget your phone.
+            </>
+        ),
+    },
+    {
+        title: 'Offline Support',
+        description: (
+            <>
+                WIFI spotty at comps? Store your scout reports and notes to upload automatically when you get connection
+                back! Don’t want to wait? Use our QR code scanning features to immediately share your results with a
+                scouting lead!
+            </>
+        )
+    },
+    {
+        title: 'Enhanced Data Aggregation',
+        description: (
+            <>
+                Perfect your robot game with our integrated picklist creator, match predictors, and AI scout report
+                summaries.
+            </>
+        ),
+    },
+    {
+        title: 'Customizable Forms',
+        description: (
+            <>
+                Don't wait for the app to update after the season begins! With the in-app form editor, scouting leads
+                can easily create forms to fit their season-specific needs.
+            </>
+        ),
+    },
+    {
+        title: 'Your data, Your choice',
+        description: (
+            <>
+                Data integrity and security is of highest priority to us. Every communication and storage area is secured and backed up as often as once per half hour.
+            </>
+        )
+    },
+    {
+        title: 'Superior Up-time',
+        description: (
+            <>
+                While we promise to stay online for years to come, if you ever have a change of heart, you can download your CSV files and move to another platform.
+            </>
+        )
+    }
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({ title, description }: FeatureItem) {
+    return (
+        <div className={clsx('col col--4')}>
+            <div className="text--center padding-horiz--md">
+                <Heading as="h3">{title}</Heading>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures(): ReactNode {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
